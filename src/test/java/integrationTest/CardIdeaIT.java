8fa28ec18d93ce680784b1f3adaa1793ff550dc0
ElijahSey <seyfarthelijah2002@gmail.com>
2023-07-30 13:53:24 +0200

    specify unit and integration tests

---
+++ src/test/java/integrationTest/CardIdeaIT.java
@@ -0,0 +1,23 @@
+package integrationTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CardIdeaIT {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+}
